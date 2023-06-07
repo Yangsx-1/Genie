146707d4f72ac5bab8d8770a0bb18ca080fb4fd1
yangsx <380129340@qq.com>
2023-06-07 20:46:22 +0800

    add parda implementation to compute reuse distance

---
+++ src/mica/eaet/parda.h
@@ -0,0 +1,70 @@
+#ifndef _PARDA_H
+#define _PARDA_H
+
+#include "splay.h"
+#include<cstdint>
+
+#define DEFAULT_NBUCKETS 1000000
+#define B_OVFL   nbuckets
+#define B_INF    nbuckets+1
+#define SLEN 20
+
+namespace mica{
+namespace parda{
+
+const size_t item_number = 4;
+class STable{
+ public:
+  STable();
+
+  struct hash_item{
+    uint64_t keyhash;
+    uint32_t last_time;
+    uint32_t total_access_time;
+  };
+
+  struct Bucket {
+    struct hash_item item_vec[item_number];
+  };
+
+  //static uint64_t make_item_vec(uint16_t tag, uint64_t item_offset);
+  uint32_t calc_bucket_index(uint64_t keyhash) const;
+  size_t get_empty_or_oldest(const Bucket* curr_bucket);
+  size_t find_same_keyhash(uint64_t keyhash, const Bucket* curr_bucket);
+  void erase_item(uint64_t keyhash, size_t bucket_index);
+  void add_item(uint64_t keyhash, uint64_t curr_time, Bucket* curr_bucket, size_t item_index);
+  //void cleanup_bucket(Bucket* bucket);
+  //void cleanup_all();
+  //size_t size();
+
+  uint32_t num_buckets_;
+  uint32_t num_buckets_mask_;
+  struct Bucket* buckets_;
+  // Padding to separate static and dynamic fields.
+  char padding0[128];
+};
+
+const int nbuckets = DEFAULT_NBUCKETS;
+
+typedef struct program_data_t {
+    STable* gh;
+    Tree* root;
+    uint32_t* histogram;
+    uint64_t n;
+    program_data_t() {
+        gh = new STable;
+        root = NULL;
+        histogram = (uint32_t*)malloc(sizeof(uint32_t) * (nbuckets+2));
+        memset(histogram, 0, (nbuckets + 2) * sizeof(uint32_t));
+        n = 0;
+    }
+};
+
+static inline void process_one_access(uint64_t keyhash, program_data_t* pdt);
+uint64_t get_pred_size(uint32_t* histogram, uint64_t sample_rate, double target, uint64_t item_size);
+void pardaStatistics(uint64_t keyhash, program_data_t* pdt);
+
+}
+}
+
+#endif
---
+++ src/mica/eaet/parda_impl.h
@@ -0,0 +1,168 @@
+#ifndef _PARDA_IMPL
+#define _PARDA_IMPL
+
+#include "parda.h"
+#include<climits>
+
+namespace mica{
+namespace parda{
+
+STable::STable()
+{
+    size_t num_buckets = 1048576;// 65536;
+    num_buckets_ = (uint64_t)num_buckets;
+    num_buckets_mask_ = (uint64_t)(num_buckets - 1);
+
+    buckets_ = (Bucket*)malloc(sizeof(Bucket) * num_buckets_);
+
+    memset(buckets_, 0, sizeof(Bucket) * (num_buckets_));
+}
+
+uint32_t STable::calc_bucket_index(uint64_t keyhash) const
+{
+    //return static_cast<uint32_t>(key_hash) & num_buckets_mask_;
+    return keyhash & num_buckets_mask_;
+}
+
+size_t STable::get_empty_or_oldest(const Bucket* curr_bucket)
+{
+    size_t item_index = 0;
+    uint64_t oldest_time = ULLONG_MAX;
+    for (size_t i = 0; i < item_number; ++i)
+    {
+        if (curr_bucket->item_vec[i].keyhash == 0) return i;//empty
+        if (oldest_time > curr_bucket->item_vec[i].last_time)
+        {
+            item_index = i;
+            oldest_time = curr_bucket->item_vec[i].last_time;
+        }
+    }
+    return item_index;
+}
+
+size_t STable::find_same_keyhash(uint64_t keyhash, const Bucket* curr_bucket)
+{
+    for (size_t i = 0; i < item_number; i++)
+    {
+        if (curr_bucket->item_vec[i].keyhash == keyhash) return i;
+    }
+    return item_number;
+}
+
+void STable::erase_item(uint64_t keyhash, size_t bucket_index)
+{
+    Bucket* bucket = this->buckets_;
+    Bucket* curr_bucket = &bucket[bucket_index];
+    for (size_t i = 0; i < item_number; i++)
+    {
+        if (curr_bucket->item_vec[i].keyhash == keyhash)
+        {
+            curr_bucket->item_vec[i].keyhash = 0;
+            curr_bucket->item_vec[i].last_time = 0;
+            curr_bucket->item_vec[i].total_access_time = 0;
+            break;
+        }
+    }
+}
+
+void STable::add_item(uint64_t keyhash, uint64_t curr_time, Bucket* curr_bucket, size_t item_index){
+    if(item_index != item_number){//find item
+        curr_bucket->item_vec[item_index].total_access_time++;
+        curr_bucket->item_vec[item_index].last_time = curr_time;
+        return;
+    }else{//didn't find item, add into hash table
+        item_index = get_empty_or_oldest(curr_bucket);
+        curr_bucket->item_vec[item_index].total_access_time = 1;
+        curr_bucket->item_vec[item_index].last_time = curr_time;
+        curr_bucket->item_vec[item_index].keyhash = keyhash;
+        return;
+    }
+}
+
+/*size_t STable::size(){
+    size_t numbers = 0;
+    for(size_t bucket_index = 0; bucket_index < num_buckets_; bucket_index++){
+        for(size_t item_index = 0; item_index < item_number; item_index++){
+            if(buckets_[bucket_index].item_vec[item_index].keyhash != 0) numbers++;
+        }
+    }
+    return numbers;
+}*/
+
+static inline void process_one_access(uint64_t keyhash, program_data_t* pdt) {
+    int distance;
+    size_t bucket_index = pdt->gh->calc_bucket_index(keyhash >> 20);
+    STable::Bucket* curr_bucket = &pdt->gh->buckets_[bucket_index];
+    size_t item_index = pdt->gh->find_same_keyhash(keyhash, curr_bucket);
+    uint32_t time = pdt->n;
+    if (item_index == item_number) {
+        pdt->root = Tinsert(time, pdt->root);
+
+        pdt->histogram[B_INF] += 1;
+        
+        pdt->gh->add_item(keyhash, time, curr_bucket, item_index);
+    }
+    // Hit: We've seen this data before
+    else {
+        int lookup = curr_bucket->item_vec[item_index].last_time;
+        pdt->root = Tinsert(lookup, pdt->root);
+        distance = node_size(pdt->root->right);
+        pdt->root = Tdelete(lookup, pdt->root);
+        pdt->root = Tinsert(time, pdt->root);
+        pdt->gh->add_item(keyhash, time, curr_bucket, item_index);
+        // Is distance greater than the largest bucket
+        if (distance > nbuckets)
+            pdt->histogram[B_OVFL] += 1;
+        else
+            pdt->histogram[distance] += 1;
+        printf("distance=%d lookup=%lu\n", distance, lookup);
+    }
+    pdt->n++;
+}
+
+uint64_t get_pred_size(uint32_t* histogram, uint64_t sample_rate, double target, uint64_t item_size){
+    int last_bucket = nbuckets - 1;
+    uint64_t sum = 0;
+    uint64_t cum = 0;
+    uint64_t dist_index;
+    while (histogram[last_bucket] == 0)
+        last_bucket--;
+    
+    for (int i = 0; i <= last_bucket; i++)
+        sum += histogram[i];
+    sum += histogram[B_OVFL];
+    sum += histogram[B_INF];
+
+    for (int i = 0; i <= last_bucket; i++) {
+        cum += histogram[i];
+        double cum_rate = 1.0 * cum / sum;
+        if(cum_rate > target){
+            dist_index = i;
+            break;
+        }
+    }
+    printf("dist=%lu rate=%lu size=%lu\n", dist_index, sample_rate, item_size);
+
+    return dist_index * sample_rate * item_size;
+}
+
+uint64_t need_sample_mask = (uint64_t(1) << 17) - 1;//2^17 - 1(128 * 1024 - 1)
+uint64_t need_sample_comp = 1024;
+uint64_t need_sample_masks[7] = {(uint64_t(1) << 10) - 1, (uint64_t(1) << 13) - 1, (uint64_t(1) << 15) - 1,
+                                (uint64_t(1) << 16) - 1, (uint64_t(1) << 17) - 1, (uint64_t(1) << 18) - 1, (uint64_t(1) << 20) - 1};
+
+bool set_sampling(uint64_t keyhash){
+    return ((keyhash & need_sample_mask) < need_sample_comp);
+}
+
+void pardaStatistics(uint64_t keyhash, program_data_t* pdt){
+    if(keyhash == 0) return;
+    if(set_sampling(keyhash)){
+        process_one_access(keyhash, pdt);
+    }
+}
+
+}
+}
+
+#endif
